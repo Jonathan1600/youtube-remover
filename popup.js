7a1dc8d1145dc4cf13075ea308472d16445153ee
Jonathan <jonathancalderonsilberman@gmail.com>
2025-12-03 02:25:13 -0700

    Base UI V1

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
 document.getElementById('remove-shorts').addEventListener("click", () => {
     console.log("CLICKED")
 })
+
+document.getElementById('ytb-rmv-on').addEventListener("click", () => {
+    const offUI = document.getElementById('off-ui')
+    const optionsElement = document.getElementById('options')
+    console.log(offUI.style.display)
+    if (!offUI.style.display || offUI.style.display == "none") {
+        offUI.style.display = "block"
+        optionsElement.style.display = "none"
+    } else {
+        offUI.style.display = "none"
+        optionsElement.style.display = "block"
+    }
+})
