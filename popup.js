55e174fa447b5eccb5076c11e59520fd24202aad
Jonathan <jonathancalderonsilberman@gmail.com>
2025-12-03 02:38:22 -0800

    Adding Local Storage

--- popup.js
+++ popup.js
@@ -1,11 +1,28 @@
-document.getElementById('ytb-rmv-on').addEventListener("click", () => {
+window.addEventListener("load", async () => {
+    const extensionOn = await chrome.storage.sync.get(["extension"])
+    const offUI = document.getElementById('off-ui')
+    const optionsElement = document.getElementById('options')
+    if (extensionOn && !!extensionOn.extension) {
+        offUI.style.display = "none"
+        optionsElement.style.display = "block"
+    } else {
+        await chrome.storage.sync.set({extension: false})
+        offUI.style.display = "block"
+        optionsElement.style.display = "none"
+    }
+});
+
+document.getElementById('ytb-rmv-on').addEventListener("click", async () => {
+    const extensionOn = await chrome.storage.sync.get(["extension"])
     const offUI = document.getElementById('off-ui')
     const optionsElement = document.getElementById('options')
     console.log(offUI.style.display)
-    if (!offUI.style.display || offUI.style.display == "none") {
+    if (extensionOn && !!extensionOn.extension) {
+        await chrome.storage.sync.set({extension: false})
         offUI.style.display = "block"
         optionsElement.style.display = "none"
     } else {
+        await chrome.storage.sync.set({extension: true})
         offUI.style.display = "none"
         optionsElement.style.display = "block"
     }
