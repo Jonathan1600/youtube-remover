509b4499a08e1f060e780952d076cbd7fc6481ce
Jonathan <jonathancalderonsilberman@gmail.com>
2025-12-03 01:57:37 -0800

    checkpoint for removing shorts

--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
-document.getElementById('remove-shorts').addEventListener("click", () => {
-    console.log("CLICKED")
-})
-
 document.getElementById('ytb-rmv-on').addEventListener("click", () => {
     const offUI = document.getElementById('off-ui')
     const optionsElement = document.getElementById('options')
@@ -13,4 +9,23 @@ document.getElementById('ytb-rmv-on').addEventListener("click", () => {
         offUI.style.display = "none"
         optionsElement.style.display = "block"
     }
+})
+
+document.getElementById("remove-shorts").addEventListener("click", async () => {
+    console.log("CLICK")
+    const [tab] = await chrome.tabs.query({active:true, currentWindow: true})
+    chrome.scripting.executeScript({
+        target: {tabID: tab.id},
+        func: () => {
+            console.log("Trying to remove shorts")
+            // Remove the Shorts carousel drawer
+            const carousels = document.querySelectorAll(
+                "ytd-rich-section-renderer, ytd-reel-shelf-renderer"
+            );
+            carousels.forEach((carousel) => {
+                // Add specific condition to target Shorts carousel if needed
+            carousel.remove();
+            });
+        }
+    })
 })
