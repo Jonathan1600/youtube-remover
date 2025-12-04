e4ee1df4ef909c340269e22e1114e403ba7bc92b
Jonathan <jonathancalderonsilberman@gmail.com>
2025-12-04 04:36:24 -0800

    Removing shorts succesfully

--- content.js
+++ content.js
@@ -1,9 +1,28 @@
-console.log("Trying to remove shorts")
-// Remove the Shorts carousel drawer
-const carousels = document.querySelectorAll(
-    "ytd-rich-section-renderer, ytd-reel-shelf-renderer, grid-shelf-view-model"
-);
-carousels.forEach((carousel) => {
-    // Add specific condition to target Shorts carousel if needed
-carousel.remove();
-})
+window.addEventListener("load", async () => {
+    const storage = await chrome.storage.sync.get(["shorts"])
+    const carousels = document.querySelectorAll(
+        "ytd-rich-section-renderer, ytd-reel-shelf-renderer, grid-shelf-view-model"
+    );
+    if (storage.shorts == false) {
+        carousels.forEach((carousel) => {
+            carousel.setAttribute("style", "display: none")
+        })
+    }
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    if(namespace == 'sync' && changes.shorts) {
+        const carousels = document.querySelectorAll(
+            "ytd-rich-section-renderer, ytd-reel-shelf-renderer, grid-shelf-view-model"
+        );
+        if (!!changes.shorts.newValue) {
+            carousels.forEach((carousel) => {
+                carousel.removeAttribute("style", "")
+            })
+        } else {
+            carousels.forEach((carousel) => {
+            carousel.setAttribute("style", "display: none")
+            })
+        }
+    }
+});
