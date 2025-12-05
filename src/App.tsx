f3cfd7891cb3ad1a698612df51760a122152c135
Jonathan <jonathancalderonsilberman@gmail.com>
2025-12-04 22:06:54 -0700

    shorts working on react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
+const eventListeners = () => {
+	window.addEventListener("load", async () => {
+		const storage = await chrome.storage.sync.get(["shorts"]);
+		const carousels = document.querySelectorAll(
+			"ytd-rich-section-renderer, ytd-reel-shelf-renderer, grid-shelf-view-model, [aria-label='Shorts'], [title='Shorts']"
+		);
+		if (storage.shorts == false) {
+			carousels.forEach((carousel) => {
+				carousel.setAttribute("style", "display: none");
+			});
+		}
+	});
+
+	chrome.storage.onChanged.addListener((changes, namespace) => {
+		if (namespace == "sync" && changes.shorts) {
+			const carousels = document.querySelectorAll(
+				"ytd-rich-section-renderer, ytd-reel-shelf-renderer, grid-shelf-view-model, [aria-label='Shorts'], [title='Shorts']"
+			);
+			if (!!changes.shorts.newValue) {
+				carousels.forEach((carousel) => {
+					carousel.removeAttribute("style");
+				});
+			} else {
+				carousels.forEach((carousel) => {
+					carousel.setAttribute("style", "display: none");
+				});
+			}
+		}
+	});
+};
+
+const doChromeScripting = async () => {
+	let [tab] = await chrome.tabs.query({
+		active: true,
+		currentWindow: true,
+	});
+	if (tab && tab.id) {
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id, allFrames: true },
+			func: eventListeners,
+		});
+	}
+};
+
 function App() {
 	const [isExtensionOn, setIsExtensionOn] = useState(false);
+	const [shortsEnabled, setShortsEnabled] = useState(true);
 
 	useEffect(() => {
-		const fetchData = async () => {
+		const fetchExtensionData = async () => {
 			const storage = await chrome.storage.sync.get(["isExtensionOn"]);
 
 			if (
@@ -19,15 +64,38 @@ function App() {
 				setIsExtensionOn(false);
 			}
 		};
+		const fetchShortsData = async () => {
+			const storage = await chrome.storage.sync.get(["shorts"]);
 
-		fetchData().catch((err) => {
-			console.error("Error fetching data from storage:", err);
+			if (storage && storage.shorts && typeof storage.shorts == "boolean") {
+				setShortsEnabled(storage.shorts);
+			} else {
+				await chrome.storage.sync.set({ shorts: true });
+				setShortsEnabled(true);
+			}
+		};
+
+		doChromeScripting().catch((err) => {
+			console.error("Error fetching data for chromeQuery:", err);
+		});
+
+		fetchExtensionData().catch((err) => {
+			console.error("Error fetching data for extension:", err);
+		});
+
+		fetchShortsData().catch((err) => {
+			console.error("Error fetching data for shorts:", err);
 		});
 	}, []);
 
 	const toggleExtension = () => {
 		chrome.storage.sync.set({ isExtensionOn: !isExtensionOn });
 		setIsExtensionOn(!isExtensionOn);
+	};
+
+	const toggleShorts = () => {
+		chrome.storage.sync.set({ shorts: !shortsEnabled });
+		setShortsEnabled(!shortsEnabled);
 	};
 
 	return (
@@ -53,7 +121,9 @@ function App() {
 			</div>
 			{isExtensionOn ? (
 				<div id="options">
-					<button id="remove-shorts">Remove Shorts</button>
+					<button id="remove-shorts" onClick={toggleShorts}>
+						Remove Shorts
+					</button>
 				</div>
 			) : (
 				<div id="off-ui">
